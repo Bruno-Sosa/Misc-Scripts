d1eaa10a265dc12422acb8243b5d5c04b5106761
Bruno-Sosa <bruno.sosa@wmail.io>
2016-04-14 17:30:00 +0200

    Update 3-Base_Arch.sh
    reverted changes (had uploaded wrong file)

--- 3-Base_Arch.sh
+++ 3-Base_Arch.sh
@@ -1,20 +1,10 @@
 #!/usr/bin/bash
 
-function install_File-System {
-         case $1 in 
-         vFAT) pacstrap -i /mnt dosfstools  ;;
-         btrfs) pacstrap -i /mnt btrfs-progs ;;
-         esac
-}
-
 installation_steps+=(install_base)
 function install_base {
          pacstrap -i /mnt base
          pacstrap -i /mnt base-devel
 
-         select fs in "vFAT EFI" "btrfs"; do
-         install_File-System $fs
-         done
 }
 
 installation_steps+=(configure_base)
