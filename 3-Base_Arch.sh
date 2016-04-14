1cc47236247149ff9c344f967cf6f4ab0c2b7c48
Bruno-Sosa <bruno.sosa@wmail.io>
2016-04-14 17:18:12 +0200

    Create 3-Base_Arch.sh

---
+++ 3-Base_Arch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+installation_steps+=(install_base)
+function install_base {
+         pacstrap -i /mnt base
+         pacstrap -i /mnt base-devel
+
+}
+
+installation_steps+=(configure_base)
+function configure_base {
+         genfstab -U /mnt >> /mnt/etc/fstab
+         nano /mnt/etc/fstab
+
+         printf "Choose a name for your computer: ";
+         read Chosen_hostname
+         echo $Chosen_hostname > /mnt/etc/hostname
+}
