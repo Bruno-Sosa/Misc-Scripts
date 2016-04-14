5188cc71e54e58ac3bf23363c7b6b8222eb6ad26
Bruno-Sosa <bruno.sosa@wmail.io>
2016-04-14 22:38:44 +0200

    Update 4-Chroots.sh
    prettify a bit

--- 4-Chroots.sh
+++ 4-Chroots.sh
@@ -10,11 +10,11 @@ function chroot_and_extra {
          # With the option to follow links turned on.
          cp -L /etc/resolv.conf "${mount_point}"/etc
 
-         mount -t proc  proc "${mount_point}"/proc
-         mount --rbind  /sys "${mount_point}"/sys
-         mount --rbind  /dev "${mount_point}"/dev
-         mount --make-rslave "${mount_point}"/sys
-         mount --make-rslave "${mount_point}"/dev
+         mount -t proc  proc    "${mount_point}"/proc
+         mount --rbind  /sys    "${mount_point}"/sys
+         mount --rbind  /dev    "${mount_point}"/dev
+         mount --make-rslave    "${mount_point}"/sys
+         mount --make-rslave    "${mount_point}"/dev
 
          chroot "${mount_point}" /bin/bash
     fi
