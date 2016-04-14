6744b4c4ce280a3e8fae9f6cc5c4bace28444dd9
Bruno-Sosa <bruno.sosa@wmail.io>
2016-04-14 16:43:51 +0200

    Update 5-Inside_Gentoo.sh

--- 5-Inside_Gentoo.sh
+++ 5-Inside_Gentoo.sh
@@ -21,3 +21,21 @@ function use_flags {
          echo MAKEOPTS=\"-j ${num}\"             >> /etc/portage/make.conf
          nano /etc/portage/make.conf
 }
+
+function kernel {
+         emerge --ask sys-kernel/gentoo-sources
+         # So that you can use `lspci`
+         emerge --ask sys-apps/pciutils
+
+         cd /usr/src/linux
+         make menuconfig
+         num=$(nproc); let num++
+         make                 -j $num #; halt
+         make modules_install -j $num
+
+         make install
+         cp /boot/vmlinuz-*-gentoo /boot/efi/EFI/bootx64.efi
+
+         emerge --ask sys-kernel/linux-firmware
+         emerge --ask app-admin/mcelog
+}
