3ffa6a366de67a1ee5cf909f236708245970b2f1
Bruno-Sosa <bruno.sosa@wmail.io>
2016-04-14 17:10:27 +0200

    Update 4-Chroots.sh
    forgot that arch-chroot did this by default (right...?)

--- 4-Chroots.sh
+++ 4-Chroots.sh
@@ -6,6 +6,10 @@ function chroot_and_extra {
     if [[ -x /bin/arch-chroot ]]; then
          arch-chroot /mnt /bin/bash
     else
+         # Copy the DNS configuration to the new system
+         # With the option to follow links turned on.
+         cp -L /etc/resolv.conf "${mount_point}"/etc
+
          mount -t proc  proc "${mount_point}"/proc
          mount --rbind  /sys "${mount_point}"/sys
          mount --rbind  /dev "${mount_point}"/dev
