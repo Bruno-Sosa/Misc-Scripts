f2e72422bdb2fad74dc0cbceca0879106e3f7f39
Bruno-Sosa <bruno.sosa@wmail.io>
2016-04-22 16:32:34 +0000

    Utilizing setting

--- 1-Start_WiFi.sh
+++ 1-Start_WiFi.sh
@@ -1,45 +1,72 @@
 #!/bin/bash
+# Usage: bash <filename>
+source Settings.sh
 
-function choose_network_interface {
-         iwconfig
-         echo "Choose a network interface: "
-         read Network_interface
-}
-
-function choose_wifi {
-         wpa_cli scan_results
-         echo "Choose a WiFi: "
-         read WiFi_name
-}
-
-installation_steps+=(start_wifi)
 function start_wifi {
          killall wpa_supplicant # Prevent already running errors
          killall dhcpcd         # Prevent already running errors
 
-         choose_network_infertace
-         
+         Start_Wifi_Settings    # If successful, will exit 
+
+         wpa_supplicant -B -i "${Network_interface}" -c "${WPA_config_file}"
+         dhcpcd
+	 exit
+}
+
+function Start_Wifi_Settings {
+
+         if [[ -z "${Network_interface}" ]]; then
+         choose_network_interface;           fi
+
+         if [[ -z "${WPA_config_file}"   ]]; then
+         create_network_configuration_1
+
+            if [[ -z "${WiFi_name}" ]]; then
+            choose_wifi               ; fi
+
+         create_network_configuration_2;     fi
+
+}
+
+function choose_network_interface {
+
+         echo "Choose a network interface: "
+         ip link | grep wl
+         read Network_interface
+}
+
+function create_network_configuration_1 {
+
          WPA_config_file='/etc/wpa_supplicant/wpa_supplicant.conf'
 
-         echo 'ctrl_interface=/var/run/wpa_supplicant' \
-            > "${WPA_config_file}"
+         echo 'ctrl_interface=/var/run/wpa_supplicant' > "${WPA_config_file}"
 
-         wpa_supplicant -B           \
-           -i "${Network_interface}" \
-           -c "${WPA_config_file}"
+         wpa_supplicant -B -i "${Network_interface}" -c  "${WPA_config_file}"
+}
+
+function choose_wifi {
 
          wpa_cli scan
-         echo "Scanning..."
-         sleep 3s
-         choose_wifi
+         echo "Scanning..."; sleep 3s
+         echo "Choose a WiFi:"
 
-         echo "This file should ONLY contain ${WiFi_name}'s password" \
-         >>   WiFi_password.txt
+         wpa_cli scan_results
+         read WiFi_name
+}
+
+function create_network_configuration_2 {
+
+         echo "The following file should ONLY contain ${WiFi_name}'s password"
+         echo "Press any key to continue..."
+         read
          nano WiFi_password.txt
          
-         wpa_passphrase "${WiFi_name}" < WiFi_password.txt \
-         >> "${WPA_config_file}"
-         
-         wpa_cli reconfigure    # Need to reload with new configuration
+         (wpa_passphrase "${WiFi_name}" < WiFi_password.txt) >> "${WPA_config_file}"
+
+         wpa_cli reconfigure # Need to reload with new configuration
+
          dhcpcd
+	 exit
 }
+
+start_wifi
